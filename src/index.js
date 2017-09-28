a1015f2b4e18ba3929cace02fc941551ea8b7f00
kirillpanov <kirill.panou@gmail.com>
2017-09-28 19:57:53 +0300

    done and tested

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets,
+      screening,
+      iteration,
+      regexp;
+  iteration = str.length;
+  for (var i = 0; i < iteration; i++) {
+    screening = [];
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      brackets = bracketsConfig[j];
+      for (var k = 0; k < brackets.length; k++) {
+        screening.push('');
+        if ( /[\[\\\^\$\.\|\?\*\+\(\)\]]/.test(brackets[0]) ) {
+          screening[k] = '\\';
+        };
+      };
+      regexp = new RegExp ( screening[0] + brackets[0] + screening[1] + brackets[1] , 'g' );
+      str = str.replace(regexp,'');
+    };
+  };
+  return str.length == 0;
 }
